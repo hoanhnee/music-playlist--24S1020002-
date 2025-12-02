0897cf1b4193c29bb02636f16b4d28420ce8eaf4
hoanhnee <mam048513@gmail.com>
2025-12-02 08:07:49 +0700

    Add files via upload

---
+++ playlist.py
@@ -0,0 +1,9 @@
+songs = []
+#-------------GIAO DIEN CHINH---------------------
+def show_menu():
+    print("\n===== MENU QUẢN LÝ PLAYLIST =====")
+    print("1. Thêm bài hát")
+    print("2. Xóa bài hát")
+    print("3. Hiển thị danh sách")
+    print("0. Thoát")
+    print("=================================")
